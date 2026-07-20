98c9865d98bd48397da724195b89a0b893ad36d2
Inn <dulsfnj413@naver.com>
2026-07-20 22:25:23 +0900

    feat: Create 기능 추가

--- src/main/java/com/example/movieapp/user/controller/UserController.java
+++ src/main/java/com/example/movieapp/user/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.example.movieapp.user.controller;
 
+import com.example.movieapp.user.dto.CreateUserRequest;
+import com.example.movieapp.user.dto.CreateUserResponse;
 import com.example.movieapp.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/users")
+    public ResponseEntity<CreateUserResponse> create(@PathVariable CreateUserRequest request) {
+        return ResponseEntity.ok(userService.create(request));
+    }
+
 }
