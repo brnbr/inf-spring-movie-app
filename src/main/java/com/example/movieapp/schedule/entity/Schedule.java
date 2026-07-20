f665f20e1f79dff3a731d17c0a612fcde68417f3
Inn <dulsfnj413@naver.com>
2026-07-21 01:01:14 +0900

    ScheduleEntity 설정

--- src/main/java/com/example/movieapp/schedule/entity/Schedule.java
+++ src/main/java/com/example/movieapp/schedule/entity/Schedule.java
@@ -1,7 +1,7 @@
 package com.example.movieapp.schedule.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.movieapp.user.entity.User;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +12,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Schedule extends BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String username;
     private String title;
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Schedule(String username, String title, String content, User user) {
+        this.username = username;
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
 }
