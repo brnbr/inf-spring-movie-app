5b8f0a35690c1d7a3e9cb8cab2a04aa88431eed3
Inn <dulsfnj413@naver.com>
2026-07-21 00:13:25 +0900

    feat: id 필드 추가

--- src/main/java/com/example/movieapp/user/dto/UpdateUserResponse.java
+++ src/main/java/com/example/movieapp/user/dto/UpdateUserResponse.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UpdateUserResponse {
 
+    private final Long id;
     private final String name;
     private final String email;
 }
