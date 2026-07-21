0209ec5661252c1cdb98f6b90d455cd88ad7e54b
Inn <dulsfnj413@naver.com>
2026-07-21 10:23:38 +0900

    chore: 안쓰는 import 삭제

--- src/main/java/com/example/movieapp/user/dto/UpdateUserReqeust.java
+++ src/main/java/com/example/movieapp/user/dto/UpdateUserReqeust.java
@@ -1,7 +1,6 @@
 package com.example.movieapp.user.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
