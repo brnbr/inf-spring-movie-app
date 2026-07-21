486d3d672a0af24f2b526ab6f40a820b8a45cd19
Inn <dulsfnj413@naver.com>
2026-07-21 09:31:21 +0900

    feat: update/delete에 password 필드 추가

--- src/main/java/com/example/movieapp/user/controller/UserController.java
+++ src/main/java/com/example/movieapp/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.movieapp.user.controller;
 
 import com.example.movieapp.user.dto.*;
 import com.example.movieapp.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,34 +15,32 @@ public class UserController {
 
     private final UserService userService;
 
-    //CREATE
     @PostMapping("/users")
-    public ResponseEntity<CreateUserResponse> create(@PathVariable CreateUserRequest request) {
+    public ResponseEntity<CreateUserResponse> create(@RequestBody CreateUserRequest request) {
         return ResponseEntity.ok(userService.create(request));
     }
 
-    //READ All
     @GetMapping("/users")
     public ResponseEntity<List<GetUserResponse>> getAllUsers() {
         return ResponseEntity.ok(userService.getAll());
     }
 
-    //READ One
     @GetMapping("/users/{userId}")
     public ResponseEntity<GetUserResponse> getOneUser(@PathVariable Long userId) {
         return ResponseEntity.ok(userService.getOne(userId));
     }
 
-    //Update
     @PutMapping("/users/{userId}")
-    public ResponseEntity<UpdateUserResponse> update(@PathVariable Long userId, @RequestBody UpdateUserReqeust reqeust) {
+    public ResponseEntity<UpdateUserResponse> update
+            (@PathVariable Long userId,
+             @RequestBody UpdateUserReqeust reqeust
+    ) {
         return ResponseEntity.ok(userService.update(userId, reqeust, reqeust.getPassword()));
     }
 
-    //Delete
     @DeleteMapping("/users/{userId}")
-    public ResponseEntity<Void> delete(@PathVariable Long userId, @RequestBody Integer password) {
-        userService.delete(userId, password);
+    public ResponseEntity<Void> delete(@PathVariable Long userId, @RequestBody PasswordRequest request) {
+        userService.delete(userId, request.getPassword());
         return ResponseEntity.noContent().build();
     }
 }
