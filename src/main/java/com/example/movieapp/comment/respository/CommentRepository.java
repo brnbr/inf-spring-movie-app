f14fc489e012d245210f4e423c806f11db11a626
Inn <dulsfnj413@naver.com>
2026-07-21 09:46:53 +0900

    feat: commentRepository 생성

--- src/main/java/com/example/movieapp/comment/respository/CommentRepository.java
+++ src/main/java/com/example/movieapp/comment/respository/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.example.movieapp.comment.respository;
 
-public interface CommentRepository {
+import com.example.movieapp.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByScheduleId(Long id);
 }
