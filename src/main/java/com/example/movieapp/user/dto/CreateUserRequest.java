736ddc44829261f586123cff897f695eab3a2b51
Inn <dulsfnj413@naver.com>
2026-07-21 09:30:17 +0900

    feat: validateon 추가

--- src/main/java/com/example/movieapp/user/dto/CreateUserRequest.java
+++ src/main/java/com/example/movieapp/user/dto/CreateUserRequest.java
@@ -1,13 +1,15 @@
 package com.example.movieapp.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CreateUserRequest {
 
     private String username;
+    @Email
     private String email;
-    private Integer password;
+    @Size(min = 8)
+    private String password;
 }
