f932dd8f4b3545ffb5dbd4dfbd2994bdb073b88a
Inn <dulsfnj413@naver.com>
2026-07-21 09:47:52 +0900

    feat: CreateResponse 생성

--- src/main/java/com/example/movieapp/comment/dto/CreateCommentResponse.java
+++ src/main/java/com/example/movieapp/comment/dto/CreateCommentResponse.java
@@ -1,4 +1,15 @@
 package com.example.movieapp.comment.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
 public class CreateCommentResponse {
+
+    private final Long id;
+    private final String content;
+    private final LocalDateTime createdAt;
 }
--- src/main/java/com/example/movieapp/comment/dto/GetCommnetResponse.java
+++ src/main/java/com/example/movieapp/comment/dto/GetCommnetResponse.java
@@ -1,4 +1,15 @@
 package com.example.movieapp.comment.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
 public class GetCommnetResponse {
+
+    private final Long id;
+    private final String content;
+    private final LocalDateTime createdAt;
 }
