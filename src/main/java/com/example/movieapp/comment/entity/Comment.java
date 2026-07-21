8f9848b5f553974536614c1aac08437d70d3ca20
Inn <dulsfnj413@naver.com>
2026-07-21 09:47:15 +0900

    feat: commentEntity 생성

--- src/main/java/com/example/movieapp/comment/entity/Comment.java
+++ src/main/java/com/example/movieapp/comment/entity/Comment.java
@@ -1,4 +1,30 @@
 package com.example.movieapp.comment.entity;
 
-public class Comment {
+import com.example.movieapp.schedule.entity.BaseEntity;
+import com.example.movieapp.schedule.entity.Schedule;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    public Comment(String content, Schedule schedule) {
+        this.content = content;
+        this.schedule = schedule;
+    }
 }
