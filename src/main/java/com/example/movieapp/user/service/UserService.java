392141170f53e1e8fb6441bf1b5d865715795363
Inn <dulsfnj413@naver.com>
2026-07-21 09:33:05 +0900

    feat: update/delete에 password 필드 추가

--- src/main/java/com/example/movieapp/user/service/UserService.java
+++ src/main/java/com/example/movieapp/user/service/UserService.java
@@ -1,25 +1,31 @@
 package com.example.movieapp.user.service;
 
+import com.example.movieapp.config.PasswordEncoder;
 import com.example.movieapp.exception.InvalidPasswordException;
 import com.example.movieapp.exception.UserNotFoundException;
 import com.example.movieapp.user.dto.*;
 import com.example.movieapp.user.entity.User;
 import com.example.movieapp.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public CreateUserResponse create(CreateUserRequest request) {
-        User user = new User(request.getUsername(), request.getEmail(), request.getPassword());
+        String encodedPasssword = passwordEncoder.encode(request.getPassword());
+
+        User user = new User(request.getUsername(), request.getEmail(), encodedPasssword);
         User saved = userRepository.save(user);
 
         return new CreateUserResponse(
@@ -33,6 +39,10 @@ public class UserService {
     @Transactional(readOnly = true)
     public List<GetUserResponse> getAll() {
         List<User> user = userRepository.findAll();
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("사용자가 없습니다.");
+        }
+
         return user.stream().map( u -> new GetUserResponse(
                 u.getId(),
                 u.getUsername(),
@@ -45,7 +55,7 @@ public class UserService {
     @Transactional(readOnly = true)
     public GetUserResponse getOne(Long userId) {
         User user = userRepository.findById(userId).orElseThrow(
-                () -> new UserNotFoundException("없는 유저입니다.")
+                () -> new UserNotFoundException("사용자가 없습니다.")
         );
 
         return new GetUserResponse(
@@ -58,12 +68,12 @@ public class UserService {
     }
 
     @Transactional
-    public UpdateUserResponse update(Long userId, UpdateUserReqeust reqeust, Integer password) {
+    public UpdateUserResponse update(Long userId, UpdateUserReqeust reqeust, String rawPassword) {
         User user = userRepository.findById(userId).orElseThrow(
-                () -> new UserNotFoundException("없는 유저입니다.")
+                () -> new UserNotFoundException("사용자가 없습니다.")
         );
 
-        if (!user.getPassword().equals(password)) {
+        if (!passwordEncoder.matches(rawPassword, user.getPassword())) {
             throw new InvalidPasswordException("비밀번호가 틀렸습니다.");
         }
 
@@ -72,12 +82,12 @@ public class UserService {
     }
 
     @Transactional
-    public void delete(Long userId, Integer password) {
+    public void delete(Long userId, String rawPassword) {
         User user = userRepository.findById(userId).orElseThrow(
-                () -> new UserNotFoundException("없는 유저입니다.")
+                () -> new UserNotFoundException("사용자가 없습니다.")
         );
 
-        if (!user.getPassword().equals(password)) {
+        if (!passwordEncoder.matches(rawPassword, user.getPassword())) {
             throw new InvalidPasswordException("비밀번호가 틀렸습니다.");
         }
 
